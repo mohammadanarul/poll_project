0cee7e3b26ee9ad43fae113a5ac4ee69ae936f00
mohammadanarul <mdanarul7075@gmail.com>
2021-01-06 23:25:40 +0600

    poll models add Question and choice models this project

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # custom application
+    'poll.apps.PollConfig',
 ]
 
 MIDDLEWARE = [
--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=255, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=150)
+    votes = models.IntegerField(default=0)
